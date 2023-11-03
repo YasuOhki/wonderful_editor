e6f98029417fb4491bfe6c33dc297f96c1313e77
Yasuhiro Ohki <>
2023-11-03 23:18:12 +0900

    Task.8 ・課題で用意されたフロントエンドのファイルを追加

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,10 @@
 module Api::V1
   class ArticlesController < BaseApiController
+    def index
+      articles = Article.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
+
     # GET /article/1
     def show
       article = Article.find(params[:id])
