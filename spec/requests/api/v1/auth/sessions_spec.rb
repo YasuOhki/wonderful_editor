519f06d9d9fef9e592d231f9042d3a3c8d66f6be
Yasuhiro Ohki <>
2023-12-05 22:46:30 +0900

    Task.9-3修正 ・let句やsubject句を用いて、テストコードの実装方法を最適化した

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -3,32 +3,34 @@ require "rails_helper"
 RSpec.describe "Api::V1::Auth::sessions", type: :request do
   describe "POST /v1/auth/sign_in" do
     subject { post(api_v1_user_session_path, params: params) }
+    let!(:first_user) { FactoryBot.create(:user) }
 
     context "正しい内容で必要な全てのパラメータを入力したとき" do
+      let!(:params) do
+        { email: first_user.email, password: first_user.password, name: first_user.name }
+      end
       it "ログインに成功する" do
-        first_user = FactoryBot.create(:user)
-        params = { email: first_user.email, password: first_user.password, name: first_user.name }
-        # subject
-        post(api_v1_user_session_path, params: params)
+        subject
         expect(response).to have_http_status(:ok) # 200
       end
     end
 
     context "入力パラメータ数は合っているがDBに登録されている内容でないとき" do
+      let!(:params) do
+        { email: "test", password: first_user.password, name: first_user.name }
+      end
       it "ログインに失敗する" do
-        first_user = FactoryBot.create(:user)
-        params = { email: "test", password: first_user.password, name: first_user.name }
-        post(api_v1_user_session_path, params: params)
+        subject
         expect(response).to have_http_status(:unauthorized) # 401
       end
     end
 
     context "入力内容は合っているが必要なカラムが不足しているとき" do
+      let!(:params) do
+        { email: first_user.email }
+      end
       it "ログインに失敗する" do
-        first_user = FactoryBot.create(:user)
-        params = { email: first_user.email }
-        # subject
-        post(api_v1_user_session_path, params: params)
+        subject
         expect(response).to have_http_status(:unauthorized) # 401
       end
     end
