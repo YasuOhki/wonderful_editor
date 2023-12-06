6500b44a39863c3f1da6bb045ec54f10ea7935fd
Yasuhiro Ohki <>
2023-12-06 16:26:52 +0900

    Task.9-4, 正常系のテストを実装

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -36,4 +36,38 @@ RSpec.describe "Api::V1::Auth::sessions", type: :request do
       end
     end
   end
+
+  describe "POST /v1/auth/sign_out" do
+    subject { delete(destroy_api_v1_user_session_path, headers: headers) }
+
+    # まずfirst_userとしてログインする
+    def user_login
+      @first_user = FactoryBot.create(:user)
+      params = { email: @first_user.email, password: @first_user.password, name: @first_user.name }
+      post(api_v1_user_session_path, params: params)
+    end
+
+    context "ログアウトに必要なヘッダ情報が揃っているとき" do
+      let(:headers) { @first_user.create_new_auth_token }
+      fit "ログアウトに成功する" do
+        user_login
+        subject
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok) # 200
+        expect(res["success"]).to eq true
+      end
+    end
+
+    context "ログアウトに必要なヘッダ情報が不足しているとき(uidがない)" do
+      it "ログアウトに失敗する" do
+
+      end
+    end
+
+    context "ログアウトしたいユーザではないユーザのヘッダ情報を入力したとき" do
+      it "ログアウトに失敗する" do
+
+      end
+    end
+  end
 end
