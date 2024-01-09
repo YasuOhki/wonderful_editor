08d3ebfbe9a037ac62cf4c62a7c710561fde0079
Yasuhiro Ohki <>
2024-01-09 23:10:23 +0900

    Task.11-3 ・模範解答を見てソースコードを修正

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -1,19 +1,16 @@
-module Api::V1::Articles
-  class DraftsController < Api::V1::BaseApiController
+module Api::V1
+  class Articles::DraftsController < BaseApiController
     before_action :authenticate_user!
 
     def index
-      binding.pry
-      drafts = current_user.articles.where(status: "draft")
+      drafts = current_user.articles.draft.order(updated_at: "DESC")    # 更新日順に並び替え
       render json: drafts,
-             each_serializer: Api::V1::Articles::DraftsPreviewSerializer
+             each_serializer: Api::V1::ArticlePreviewSerializer
     end
 
     def show
-      binding.pry
-      draft_article = Article.find(params[:id])
-      #draft_article = current_user.articles.find(params[:id])
-      render json: draft_article, serializer: Api::V1::Articles::DraftsDetailSerializer
+      draft_article = current_user.articles.draft.find(params[:id])
+      render json: draft_article, serializer: Api::V1::ArticleDetailSerializer
     end
 
     private
--- app/serializers/api/v1/articles/drafts_preview_serializer.rb
+++
@@ -1,4 +0,0 @@
-class Api::V1::Articles::DraftsPreviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :created_at, :status
-  belongs_to :user, serializer: Api::V1::UserSerializer
-end
